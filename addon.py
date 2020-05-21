b6bdafe4c479bfa8cc05c1eacfb3aa1ecfb37893
qxz0kpi <Nuno.Neto@ctw.bmwgroup.com>
2020-05-21 12:56:56 +0100

    Updated Path

--- addon.py
+++ addon.py
@@ -1,6 +1,6 @@
 import subprocess
 
-steamLink = "/usr/bin/steamlink %U"
+steamLink = "/usr/bin/steamlink"
 
 process = subprocess.Popen(steamLink, stdout=subprocess.PIPE)
 output, error = process = process.communicate()
